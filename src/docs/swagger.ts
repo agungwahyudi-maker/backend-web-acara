f9f21ac604b15e6450affadb40102c2191151ef8
agungwahyudi-maker <agungwahyu0122@gmail.com>
2025-02-01 19:19:46 +0700

    swagger

---
+++ src/docs/swagger.ts
@@ -0,0 +1,40 @@
+import swaggerAutogen from "swagger-autogen";
+
+const doc = {
+  info: {
+    version: "1.0.0",
+    title: "Dokumentasi API MERN Stack",
+    description: "API Documentation",
+  },
+  servers: [
+    {
+      url: "http://localhost:3000/api",
+      description: "Development Local server",
+    },
+    {
+      url: "https://backend-web-acara-2025.vercel.app/api",
+      description: "Deploy server",
+    },
+  ],
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: "http",
+        scheme: "bearer",
+      },
+    },
+    schemas: {
+      LoginRequest: {
+        identifier: "agung",
+        password: "agung",
+      },
+    },
+  },
+};
+
+const outputFile = "./swagger_output.json";
+const endpointsFiles = ["../routes/api.ts"];
+
+swaggerAutogen({
+  openapi: "3.0.0",
+})(outputFile, endpointsFiles, doc);
--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,21 @@ import bodyParser from "body-parser";
 import express from "express";
 import router from "./routes/api";
 import db from "./utils/database";
+import docs from "./docs/route";
+import cors from "cors";
 
 async function init() {
   try {
     const result = await db();
     console.log("Database status :", result);
     const app = express();
+    app.use(cors());
     const PORT = 3000;
     app.use(bodyParser.json());
 
     app.use("/api", router);
+    docs(app);
+
     app.get("/", (req, res) => {
       res.status(200).json({
         message: "Welcome to my API",
